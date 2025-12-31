c69495a74fe0ea1582edd33846569c0a0e0084d7
Hazem <217106468+someone-in-a-hat@users.noreply.github.com>
2025-12-31 20:18:06 +0200

    Delete directory

--- root/dist/mock.js
+++
@@ -1,7 +0,0 @@
-(function () {
-	'use strict';
-
-
-
-})();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9jay5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7In0=
